3c891935cae81e2ae5a9706556b15893a64d7d9e
Vinayagam <vinayagam.thaagamfoundation@gmail.com>
2026-01-28 09:15:56 +0530

    Configure PostgreSQL for Render

--- config/settings.py
+++ config/settings.py
@@ -76,14 +76,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
+import dj_database_url
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
