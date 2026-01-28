f957e30993f826969b34b8e5ec0d3e9360cc5341
Vinayagam <vinayagam.thaagamfoundation@gmail.com>
2026-01-28 10:00:41 +0530

    Update superuser credentials to vinay

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ echo "Running migrate..."
 python manage.py migrate
 
 echo "Creating superuser..."
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin123')"
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='vinay').exists() or User.objects.create_superuser('vinay', 'vinay@example.com', 'Vinay@123')"
 
 echo "Starting Server..."
 gunicorn config.wsgi:application
